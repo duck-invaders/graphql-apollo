b0a4af05429db8f51fe06d26e471a29097b19d30
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2018-04-11 15:01:49 +0200

    feat: add engine

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { ApolloEngine } from 'apollo-engine';
 import schema from './schemas';
 
 const PORT = 3000;
@@ -11,7 +12,19 @@ app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql'
 }));
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema,
+    tracing: true,
+    cacheControl: true
+}));
 
 
-app.listen(PORT);
+const engine = new ApolloEngine({
+    apiKey: 'service:CaptainJojo-9556:zYx_pkxqMfES2wOKFuZ5mA'
+});
+
+engine.listen({
+    port: PORT,
+    expressApp: app,
+});
+
